367f8162d4aed327b319c6b2cc63dbc85dc748a5
Pedrat <po.matrix@gmail.com>
2018-02-26 13:20:51 +0000

    FuckThisv3

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def send_message(recipient_id, message_text):
             "id": recipient_id
         },
         "message": {
-            "text": message_text
+            "text": message_text+' -bot'
         }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
