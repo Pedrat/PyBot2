17679b11ca9ed26ef6c45724ef16dbbac77f21e3
Pedrat <Pedrat@users.noreply.github.com>
2018-02-26 21:08:41 +0000

    Limpeza

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
 # coding=utf-8
-import os
-import sys
-import json
-import random
-import fb
-#import facebook
+import os,sys,json,random,fb
 from fbmq import Page , Template, Attachment, QuickReply
 from datetime import datetime
-#listasender=["1838746479497346"]
-page = Page("EAACoZCnVve74BAAIZCs17iPNPK6pUatUdOKhY2EciLVhTEZAU2Bx1KD3EFYiUvYtFYxNXEOQXYj2VVcme8PmsLBuHQGQgDztJfcjcqVPZBfM8ZArrXgOxvSbgvrUZAIvz34ACTZBhUUfQ6qrlY7KHEN0lBZAng5Oylz58XGtGfmJAd2l9bE4sjS5")
-#moderators=[]
-#token= 351316092049412
-#facebook=fb.graph.api(token)
 import requests
 from flask import Flask, request
+page = Page("EAACoZCnVve74BAAIZCs17iPNPK6pUatUdOKhY2EciLVhTEZAU2Bx1KD3EFYiUvYtFYxNXEOQXYj2VVcme8PmsLBuHQGQgDztJfcjcqVPZBfM8ZArrXgOxvSbgvrUZAIvz34ACTZBhUUfQ6qrlY7KHEN0lBZAng5Oylz58XGtGfmJAd2l9bE4sjS5")
 date=datetime.now().strftime("%d/%m")
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
+    # Vai ao endpoint e verifica os tokens, para o webhook
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
@@ -32,39 +22,34 @@ def verify():
 @app.route('/', methods=['POST'])
 def webhook():
 
-    # endpoint for processing incoming messaging events
+    # Processa msg
 
     data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    log(data)
 
     if data["object"] == "page":
-
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
                 exemplos = ["Peço imensa desculpa, não pense que sou um bot burro.....DITO ISTO.... Não faço ideia do que disse... sorry :D","Não sei essa palavra :c Desculpa! Mas os nossos donos foram avisados!","Bolas, peço imensa desculpa mas não consigo lhe ajudar..."]
-                if messaging_event.get("message"):  # someone sent us a message
+                if messaging_event.get("message"):  # Alguem mandou algo
                     if messaging_event['message'].get('text'):
-                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                        message_text = messaging_event["message"]["text"]  # the message's text
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID da pessoa
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID da pagina
+                        message_text = messaging_event["message"]["text"]  # the text
                         if message_text == "Ajuda!":
                             page.send(sender_id,"Com o que podemos ajudar?")
                         elif message_text == "Que dia e hoje?":
                             page.send(sender_id,("{}".format(datetime.now().strftime("%d/%m/%Y"))))
                         elif message_text == "Publica-me isto sff":
                             page.send(sender_id,"Ok! :D")
-                            #friends_and_education=facebook.get_object(cat='single', id='me', fields=['friends', 'education'])
                             print friends_and_education
-                            #facebook.publish(cat="feed", id="me", message="My facebook status")
                         else:
                             msg = random.choice(exemplos)
                             page.send(sender_id, msg)
                     if messaging_event['message'].get('attachments'):
-                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                        teste=messaging_event["message"]["attachments"]
-
-
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID da pessoa
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID da pagina
+                        teste=messaging_event["message"]["attachments"] #Para ver o type
                         if 'image' in str(teste[0]):
                             msg=get_message('image')
                         elif 'file' in str(teste[0]):
@@ -75,7 +60,6 @@ def webhook():
                             msg=get_message('audio')
                         else:
                             msg = "Já o vou ver"
-                        #msg = get_message()
                         page.send(sender_id,msg)
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -88,7 +72,7 @@ def webhook():
 
     return "ok", 200
 
-
+'''
 def send_message(recipient_id, message_text):
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
@@ -111,7 +95,7 @@ def send_message(recipient_id, message_text):
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
-
+'''
 
 def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
     try:
@@ -124,7 +108,7 @@ def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
         pass  # squash logging errors in case of non-ascii text
     sys.stdout.flush()
 
-def get_message(tipo):
+def get_message(tipo): #Random msg
     if tipo == 'image':
         exemplos= ["Lindo/a","Que giro","Wow"]
     if tipo == 'video':
