81714b469a83161afd17693ee4edb299e61a8aa4
Pedrat <po.matrix@gmail.com>
2018-03-02 08:57:51 +0000

    sdq

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from flask import Flask, request
 from botheader import buttons, Handle, quickReply
 import botheader
 
-page = botheader.page
-app = Flask(__name__)
+page = botheader.page #Alias
+app = Flask(__name__) #Guarda o flask em uma app
 
 @app.route('/', methods=['GET'])
 def verify():
-    # Vai ao endpoint e verifica os tokens, para o webhooksad
+    # Vai ao endpoint e verifica os tokens, para o webhook
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
@@ -20,13 +20,13 @@ def verify():
 
     return "Hello world", 200
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST']) #Caso exista um POST no index do webhook chama a função e decorater seguintes
 def webhook():
-    page.greeting("Bem vindo, a nossa loja de produtos recreativos, por favor, pergunte-me algo!")
-    page.show_starting_button("START_PAYLOAD")
-    page.show_persistent_menu([Template.ButtonWeb('Website', 'http://www.cwstudio.pt/')])
-    payload = request.get_data(as_text=True)
-    print(payload)
+    page.greeting("Bem vindo, a nossa loja de produtos recreativos, por favor, pergunte-me algo!") #Info da pagina que o bot disponibiliza
+    page.show_starting_button("START_PAYLOAD") #Butão de começar que me deu uma dor de cabeça do clrh
+    page.show_persistent_menu([Template.ButtonWeb('Website', 'https://www.leafly.com/')]) #Mostra um menu todo pimposo ao lado
+    payload = request.get_data(as_text=True) #Faz um request ao webhook e obtem a data
+    print(payload) #Cria o log
     # Processa msg
     page.handle_webhook(payload)
     return "ok", 200
--- botheader.py
+++ botheader.py
@@ -51,10 +51,10 @@ class quickReply:
             return random.choice(playlist)
 
 class Handle:
-    def get_num():
+    def get_num(): #Obtem um numero random entre 1 e 2
         numbergen=[1,2]
         return random.choice(numbergen)
-    def get_att(tipo):
+    def get_att(tipo): #Obtem uma imagem random para enviar
         if tipo == 'image':
             exemplos = ["https://cdn.shopify.com/s/files/1/0862/4240/products/1_0d691e32-3771-402a-aaee-dc004ea1b2c3.jpeg?v=1441091543","https://vignette.wikia.nocookie.net/harrypotter/images/2/27/Happy-guy-thumbs-up-300x237.gif/revision/latest?cb=20121019041406"]
 
@@ -62,7 +62,7 @@ class Handle:
             exemplos =["http://static.twentytwowords.com/wp-content/uploads/Thumbs-and-Ammo-02.jpg","http://4.bp.blogspot.com/-EGzuN7Jcj0I/UUnR1Y0xWQI/AAAAAAAAA2Q/XMK6_yMNYPo/s1600/ChuckNorristhumbsup+Emil+P.jpg"]
         return random.choice(exemplos)
 
-    def get_message(tipo): #Random msgs
+    def get_message(tipo): #Obtem uma msg random para enviar
         if tipo == 'image':
             exemplos= ["Lindo/a","Que giro","Wow"]
         elif tipo == 'video':
@@ -76,54 +76,52 @@ class Handle:
         return (random.choice(exemplos)+' -signed bot')
 
 @page.handle_message
-def message_handler(event):
-    sender_id = event.sender_id
-    timestamp = event.timestamp
-    message = event.message
-    page.typing_on(sender_id)
-    page_id = page.page_id
-    page_name = page.page_name
-    user_profile = page.get_user_profile(event.sender_id) # return dict
-    nomeuser=(user_profile.get("first_name")+" "+user_profile.get("last_name"))
-    #print(page_id)
-    #print(page_name)
+def message_handler(event): #Trabalha as msg
+    sender_id = event.sender_id #O id da pessoa que envia a msg
+    timestamp = event.timestamp #timestamp
+    message = event.message #A mensagem
+    page.typing_on(sender_id) #Faz com que aquelas bolinhas fancy aparecam
+    page_id = page.page_id #O id da nossa pagina
+    page_name = page.page_name #O nome da nossa pagina
+    user_profile = page.get_user_profile(sender_id) #Infos do user em formato de dicionario
+    nomeuser=(user_profile.get("first_name")+" "+user_profile.get("last_name")) #Log sobre o user
     print(user_profile)
-    if message.get("attachments"):
-        if 'image' in str(message.get("attachments")):
-            if '369239263222822' in str(message.get("attachments")):
+    if message.get("attachments"): #se a msg é um attachments
+        if 'image' in str(message.get("attachments")): #Se e uma imaghem
+            if '369239263222822' in str(message.get("attachments")): #Se e o fixezinho
                 image_url=Handle.get_att('thumbs')
                 page.send(sender_id,Attachment.Image(image_url))
-            else:
-                if Handle.get_num() == 1:
+            else: #Imagem normal
+                if Handle.get_num() == 1: #Envia txt
                     msg=Handle.get_message('image')
                     page.send(sender_id,msg)
-                else:
+                else: #Envai imagem
                     image_url=Handle.get_att('image')
                     page.send(sender_id,Attachment.Image(image_url))
-        elif 'video' in str(message.get("attachments")):
+        elif 'video' in str(message.get("attachments")): #Se for video
             msg=Handle.get_message('video')
             page.send(sender_id,msg)
-        elif 'audio' in str(message.get("attachments")):
+        elif 'audio' in str(message.get("attachments")): #Se for audio
             msg=Handle.get_message('audio')
             page.send(sender_id,msg)
-        elif 'file' in str(message.get("attachments")):
+        elif 'file' in str(message.get("attachments")): #Se for file
             page.send(sender_id,"Files são dubios")
-        else:
+        else: #Fault tolerance
             page.send(sender_id,"Já o vou ver! :D")
-    elif message.get("quick_reply"):
-        if "PICK_MENU" in str(message.get("quick_reply")):
+    elif message.get("quick_reply"): #Se for um quick_reply
+        if "PICK_MENU" in str(message.get("quick_reply")): #Se tiver escolhido o menu
             page.send(sender_id,Template.Buttons("Nosso menu",buttons.btnmenu))
-        elif "PICK_MUS" in str(message.get("quick_reply")):
+        elif "PICK_MUS" in str(message.get("quick_reply")): #Se tiver escolhido o menu de musica
             page.send(sender_id,"Qual é o seu genero de música favorito?",quick_replies=quickReply.quick_musica,metadata="TEST")
-        elif "PICK_PRECO" in str(message.get("quick_reply")):
+        elif "PICK_PRECO" in str(message.get("quick_reply")): #Se tiver escolhido o Preço
             page.send(sender_id,"Deste momento não disponibilizamos preços. Obrigado.")
-        else:
+        else: #Se for de um genero musical
             video_url=quickReply.get_music((message.get("quick_reply")).get('payload'))
             page.send(sender_id,video_url)
-    elif message.get("text"):
-        message = event.message_text
+    elif message.get("text"): #Se for texto
+        message = event.message_text #Guarda o texto
         print(message)
-        message = unicodedata.normalize('NFKD', message).encode('ASCII', 'ignore').decode().lower()
+        message = unicodedata.normalize('NFKD', message).encode('ASCII', 'ignore').decode().lower() #nomralliza a string
         if message.upper() in smile:
             page.send(sender_id,Handle.get_message('smile'))
         elif message in QuestaoPreco:
@@ -142,28 +140,28 @@ def message_handler(event):
            page.send(sender_id, "questões mais especificas serão remetidas para os administradores da págida e respondidas com a maior brevidade possivel")
         elif message == "menu":
             page.send(sender_id,Template.Buttons("Nosso menu",buttons.btnmenu))
-        else:
+        else: #Mensagem default caso o bot nao saiba o que fazer
             msg = Handle.get_message('text')
             page.send(sender_id,msg,quick_replies=quickReply.default_menu,metadata="TEST")
-            for x in moderator:
+            for x in moderator: #manda para os donos a dizer que houve porcaria
                 page.send(x,"Ocorreu um problema hoje, não soube responder a algo que o {} perguntou-me. Sorry :(".format(nomeuser))
-@page.handle_postback
+@page.handle_postback #Quando recebe um postback
 def received_postback(event):
     sender_id = event.sender_id
     recipient_id = event.recipient_id
     time_of_postback = event.timestamp
-    payload = event.postback_payload
-    print("Postback de {} recebido. Payload de :{}".format(sender_id,payload))
-    if payload == "START_PAYLOAD":
+    payload = event.postback_payload #Obtem a payload
+    print("Postback de {} recebido. Payload de :{}".format(sender_id,payload)) #LOG
+    if payload == "START_PAYLOAD": #Se o get started ter sido pressionado
         page.send(sender_id,Template.Buttons("Nosso menu",buttons.btnmenu))
-    elif payload == "MUSIC_PAYLOAD":
+    elif payload == "MUSIC_PAYLOAD": #Se o butao de musica ser pressionado
         page.send(sender_id,"Qual é o seu genero de música favorito?",quick_replies=quickReply.quick_musica,metadata="TEST")
-    elif payload == "AJUDA_PAYLOAD":
+    elif payload == "AJUDA_PAYLOAD": #Se for de ajuda
         page.send(sender_id,"Pode-me dizer, a qualquer altura 'menu' e eu irei te mostrar o menu! :D")
-    elif payload == "PROD_PAYLOAD":
-        page.send(sender_id,"Vai de 10€ até a 100€")
+    elif payload == "PROD_PAYLOAD": #Se for de "Produtos recreativos"
+        page.send(sender_id,"Temos uma variada range de produtos.\nCarregue no menu ao seu lado esquerdo, para ir ao nosso website")
 
-@page.handle_delivery
+@page.handle_delivery #Para verificar que a msg e enviada com sucesso
 def received_delivery_confirmation(event):
     delivery = event.delivery
     message_ids = delivery.get("mids")
@@ -171,6 +169,6 @@ def received_delivery_confirmation(event):
         for message_id in message_ids:
             print("O user recebeu a msg nº %s" % message_id)
 
-@page.handle_read
+@page.handle_read #Para verificar se o user le a msg 
 def received_message_read(event):
     print("O user leu a mensagem")
